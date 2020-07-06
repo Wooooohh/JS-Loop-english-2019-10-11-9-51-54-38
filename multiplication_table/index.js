e6fabe5ccfc900302e198c19f4dbc7f93b01777b
Wooooohh <42633990+Wooooohh@users.noreply.github.com>
2020-07-06 12:49:02 +0800

    Create index.js
    write js code

---
+++ multiplication_table/index.js
@@ -0,0 +1,8 @@
+<script type = "text/javascript">
+for(var i=1;i<=9;i++){
+    for(var j = 1; j <= i; j++){
+         document.write(" " + i + "*" + j + "=" + i*j + "");
+    }
+    document.write("<br/>");
+}
+</script>
