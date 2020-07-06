33b0604de0cdaaa9a507df541a5f14ff4d7f7512
Wooooohh <42633990+Wooooohh@users.noreply.github.com>
2020-07-06 12:54:02 +0800

    Create index.js
    write js code

---
+++ number/index.js
@@ -0,0 +1,9 @@
+<script text = "text/javascript">
+  for(var i = 1; i <= 20; i++){
+      if(i%2 == 0){
+        document.write(i + " is even number." )
+      }else{
+        document.write(i + " is odd number." )
+      }
+  }
+</script>
